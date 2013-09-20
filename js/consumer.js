bff15bed8a218ea6dfd9dc4872268ef305f88882
chrkolst <christopher.kolstad@finn.no>
2013-09-20 15:39:23 +0200

    Add json test

--- js/consumer.js
+++ js/consumer.js
@@ -5,7 +5,16 @@ $(document).ready(function () {
 	}).done(function (data) {
 		$.each(data.ads, function (idx, ad) {
 			var holder = $("<div></div>").text("idx: " +idx + "\tid: " +ad.id + "\ttitle:" +ad.title);
-			$("#adholder").append(holder);
+			$("#jsonPAdholder").append(holder);
 		});
 	});
+	$.ajax({
+		url:"http://frozen-falls-4637.herokuapp.com/",
+		dataType:"json"
+	}).done(function (data) {
+		$.each(data.ads, function (idx, ad) {
+			var holder = $("<div></div>").text("idx: " +idx + "\tid: " +ad.id + "\ttitle:" +ad.title);
+			$("#jsonAdholder").append(holder);
+		})
+	})
 })
