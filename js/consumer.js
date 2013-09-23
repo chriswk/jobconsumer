2e54aa6683a882ede4fdb4b7ab30d306f0b7720f
chrkolst <christopher.kolstad@finn.no>
2013-09-23 15:38:08 +0200

    Added xml endpoint and decide response by path for now

--- js/consumer.js
+++ js/consumer.js
@@ -1,20 +1,35 @@
+function parseData(placeholder, data) {
+	$.each(data.ads, function (idx, ad) {
+		var holder = $("<div></div>").text("idx: " +idx + "\tid: " +ad.id + "\ttitle:" +ad.title);
+		$(placeholder).append(holder);
+	});	
+}
+
+function getNode(node, name) {
+	console.log(node.getElementsByTagName(name)[0].textContent);
+	return node.getElementsByTagName(name)[0].textContent;
+}
 $(document).ready(function () {
 	$.ajax({
-		url:"http://frozen-falls-4637.herokuapp.com/",
+		url:"http://frozen-falls-4637.herokuapp.com/json",
 		dataType:"jsonp"
 	}).done(function (data) {
-		$.each(data.ads, function (idx, ad) {
-			var holder = $("<div></div>").text("idx: " +idx + "\tid: " +ad.id + "\ttitle:" +ad.title);
-			$("#jsonPAdholder").append(holder);
-		});
+		parseData("#jsonPAdholder", data);
 	});
 	$.ajax({
-		url:"http://frozen-falls-4637.herokuapp.com/",
+		url:"http://frozen-falls-4637.herokuapp.com/json",
 		dataType:"json"
 	}).done(function (data) {
-		$.each(data.ads, function (idx, ad) {
-			var holder = $("<div></div>").text("idx: " +idx + "\tid: " +ad.id + "\ttitle:" +ad.title);
-			$("#jsonAdholder").append(holder);
+		parseData("#jsonAdholder", data);
+	});
+	
+	$.ajax({
+		url:"http://frozen-falls-4637.herokuapp.com/xml",
+		dataType:"xml"
+	}).done(function (data) {
+		$.each(data.getElementsByTagName("response"), function (idx, el) {
+			var holder = $("<div></div>").text("idx: " +idx +"\tid: " +getNode(el, "id") +"\ttitle: " +getNode(el, "title"));
+			$("#xmlAdHolder").append(holder);
 		})
-	})
+	});
 })
